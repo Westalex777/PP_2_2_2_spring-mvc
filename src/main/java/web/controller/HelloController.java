625c3e37175f2ade01847a52d7a2e193ab02be54
Westalex777 <westalex777@yandex.ru>
2023-01-10 23:05:23 +0600

    commit

--- src/main/java/web/controller/HelloController.java
+++ src/main/java/web/controller/HelloController.java
@@ -1,24 +1,33 @@
 package web.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import web.service.CarService;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class HelloController {
 
-	@GetMapping(value = "/")
-	public String printWelcome(ModelMap model) {
-		List<String> messages = new ArrayList<>();
-		messages.add("Hello!");
-		messages.add("I'm Spring MVC application");
-		messages.add("5.2.0 version by sep'19 ");
-		model.addAttribute("messages", messages);
-		return "index";
-	}
-	
+    @GetMapping("/")
+    public String printWelcome(ModelMap model) {
+        List<String> messages = new ArrayList<>();
+        messages.add("Hello!");
+        messages.add("I'm Spring MVC application");
+        messages.add("5.2.0 version by sep'19 ");
+        model.addAttribute("messages", messages);
+        return "index";
+    }
+
+    @GetMapping("/cars")
+    public String printCars(Model model, @RequestParam(value = "count", required = false) Optional<Integer> count) {
+        CarService carService = new CarService();
+        model.addAttribute("cars", carService.getCars(count.orElse(5)));
+        return "cars";
+    }
 }
---
+++ src/main/java/web/service/CarService.java
@@ -0,0 +1,20 @@
+package web.service;
+
+import web.model.Car;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class CarService {
+
+    private List<Car> cars = Arrays.asList(
+            new Car(1, "Mercedes", "White"),
+            new Car(2, "BMW", "Black"),
+            new Car(3, "Audi", "Grey"),
+            new Car(4, "Lada", "Red"),
+            new Car(5, "KIA", "White"));
+
+    public List<Car> getCars(int count) {
+        return cars.stream().limit(count).toList();
+    }
+}
